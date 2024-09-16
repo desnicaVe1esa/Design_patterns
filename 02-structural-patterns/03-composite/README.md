cc9ac49cccd27401c62b0d5ff8c4924fed098e69
Kirill <seeraydary@gmail.com>
2024-09-16 18:57:43 +0300

    02-structural-patterns.03-composite

---
+++ 02-structural-patterns/03-composite/README.md
@@ -0,0 +1,5 @@
+![composite.jpg](composite.jpg)
+
+![composite-structure.jpg](composite-structure.jpg)
+
+![composite-example.jpg](composite-example.jpg)
---
+++ 02-structural-patterns/03-composite/code/01-Shape.java
@@ -0,0 +1,24 @@
+/**
+ * Общий интерфейс компонентов.
+ */
+public interface Shape {
+    int getX();
+
+    int getY();
+
+    int getWidth();
+
+    int getHeight();
+
+    void move(int x, int y);
+
+    boolean isInsideBounds(int x, int y);
+
+    void select();
+
+    void unSelect();
+
+    boolean isSelected();
+
+    void paint(Graphics graphics);
+}
