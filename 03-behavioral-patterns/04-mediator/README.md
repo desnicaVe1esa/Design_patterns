df4326a64935b1501542b883d4fe13f7ec6b5a20
Kirill <seeraydary@gmail.com>
2024-09-17 16:49:47 +0300

    03-behavioral-patterns.04-mediator

---
+++ 03-behavioral-patterns/04-mediator/README.md
@@ -0,0 +1,5 @@
+![mediator.jpg](mediator.jpg)
+
+![mediator-structure.jpg](mediator-structure.jpg)
+
+![mediator-example.jpg](mediator-example.jpg)
---
+++ 03-behavioral-patterns/04-mediator/code/09-Mediator.java
@@ -0,0 +1,16 @@
+/**
+ * Общий интерфейс посредников.
+ */
+public interface Mediator {
+    void addNewNote(Note note);
+    void deleteNote();
+    void getInfoFromList(Note note);
+    void saveChanges();
+    void markNote();
+    void clear();
+    void sendToFilter(ListModel listModel);
+    void setElementsList(ListModel list);
+    void registerComponent(Component component);
+    void hideElements(boolean flag);
+    void createGUI();
+}
